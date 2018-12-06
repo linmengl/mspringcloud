071674c0e1eae8a407d46d50310efa923fa53359
linmengl <319211mlkl>
2018-12-06 22:20:59 +0800

    config

---
+++ mspringcloud-config-client-3355/src/main/java/com/btc/springcloud/rest/ConfigClient_StartApp.java
@@ -0,0 +1,11 @@
+package com.btc.springcloud.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClient_StartApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClient_StartApp.class,args);
+    }
+}
